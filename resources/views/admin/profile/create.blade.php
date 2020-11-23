d28177ed0395b72cf0ba760c00e7881061ad7897
”naiteru3” <naiteru33@yahoo.co.jp>
2020-11-23 06:33:42 +0000

    課題 13

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,48 @@
 @extends('layouts.profile')
 
-@section('title', '珍しいニュースの新規作成')
+@section('title', '自己紹介ページ')
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>新規ニュース新規作成</h2>
+                <h2>自己紹介</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" name="name" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" name="gender" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                   {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="送信">
+                </form>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('news/create', 'Admin\NewsController@add');
-     
-     
      Route::post('news/create', 'Admin\NewsController@create'); 
      
      Route::get('profile/create', 'Admin\ProfileController@add');
+     Route::post('profile/create', 'Admin\ProfileController@create');
      Route::get('profile/edit', 'Admin\ProfileController@edit');
+     Route::post('profile/edit', 'Admin\ProfileController@update');
      
 });
 
